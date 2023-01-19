b16f13c7046e4d45269369ae5bd85b1e53691984
sai <tshsai0@gmail.com>
2023-01-19 18:29:42 +0530

    second commit

--- cart.js
+++ cart.js
@@ -5,20 +5,25 @@ let pro1_add=document.getElementById("add_pro1");
 let pro2_add=document.getElementById("add_pro2");
 let pro1_delete=document.getElementById("delete_pro1");
 let pro2_delete=document.getElementById("delete_pro2");
+let final=0;
 pro1_add.addEventListener('click',(e)=>{
     e.preventDefault();
     cart_count=parseInt(cart_count)+1;
     pro1_count=parseInt(pro1_count)+1;
+    final=parseInt(final)+parseInt(130000);
     document.getElementById("cart").innerHTML=cart_count;
     document.getElementById("pro1").innerHTML=pro1_count;
+    document.getElementById("total").innerHTML=final;
 
 })
 pro2_add.addEventListener('click',(e)=>{
     e.preventDefault();
     cart_count=parseInt(cart_count)+1;
     pro2_count=parseInt(pro2_count)+1;
+    final=parseInt(final)+parseInt(100000);
     document.getElementById("cart").innerHTML=cart_count;
     document.getElementById("pro2").innerHTML=pro2_count;
+    document.getElementById("total").innerHTML=final;
 
 })
 pro1_delete.addEventListener('click',(e)=>{
@@ -26,9 +31,12 @@ e.preventDefault();
 if((parseInt(pro1_count)-1)>=0){
     cart_count=parseInt(cart_count)-1;
     pro1_count=parseInt(pro1_count)-1;
+    final=parseInt(final)-parseInt(130000);
+    document.getElementById("total").innerHTML=final;
 }
 document.getElementById("cart").innerHTML=cart_count;
 document.getElementById("pro1").innerHTML=pro1_count;
+
 })
 
 pro2_delete.addEventListener('click',(e)=>{
@@ -36,6 +44,8 @@ pro2_delete.addEventListener('click',(e)=>{
     if((parseInt(pro2_count)-1)>=0){
         cart_count=parseInt(cart_count)-1;
         pro2_count=parseInt(pro2_count)-1;
+        final=final-100000;
+        document.getElementById("total").innerHTML=final;
     }
     document.getElementById("cart").innerHTML=cart_count;
     document.getElementById("pro2").innerHTML=pro2_count;
